9f472ae1569f5fbed6dd529e2abb30627c2dedff
Misha Levko <lvk.misha@gmail.com>
2022-09-12 01:40:30 +0300

    finish ex-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const start = document.querySelector('button[data-start]');
+const stop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
 
+let timerColor = null;
+stop.setAttribute('disabled', true);
+
+start.addEventListener('click', startClick);
+stop.addEventListener('click', stopClick);
+
+function startClick() {
+  timerColor = setInterval(() => {
+    const color = getRandomHexColor();
+    body.style.backgroundColor = `${color}`;
+  }, 1000);
+  start.setAttribute('disabled', true);
+  stop.removeAttribute('disabled');
+}
+
+function stopClick() {
+  clearInterval(timerColor);
+  start.removeAttribute('disabled');
+  stop.setAttribute('disabled', true);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
