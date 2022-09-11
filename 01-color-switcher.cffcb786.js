57523e27e6a08a5b09c964eea86e185b9c9a8410
MishaLvk <99795833+MishaLvk@users.noreply.github.com>
2022-09-11 22:41:03 +0000

    Deploying to gh-pages from @ MishaLvk/goit-js-hw-09@9f472ae1569f5fbed6dd529e2abb30627c2dedff 🚀

---
+++ 01-color-switcher.cffcb786.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");let o=null;e.setAttribute("disabled",!0),t.addEventListener("click",(function(){o=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16)}`;r.style.backgroundColor=`${t}`}),1e3),t.setAttribute("disabled",!0),e.removeAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(o),t.removeAttribute("disabled"),e.setAttribute("disabled",!0)}));
+//# sourceMappingURL=01-color-switcher.cffcb786.js.map
